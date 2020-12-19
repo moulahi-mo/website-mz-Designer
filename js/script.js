f058d9c4bd65d0de937e0f0ba3a051e50a8b5ffa
moulahi-mo <moulahi.mo@gmail.com>
2020-12-20 00:14:50 +0100

    adding actually work section

--- js/script.js
+++ js/script.js
@@ -5,3 +5,33 @@ document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, options);
 });
+
+//* modal
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, options);
+});
+//* scrollspy
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.scrollspy');
+  var instances = M.ScrollSpy.init(elems, options);
+});
+var instance = M.ScrollSpy.getInstance(elem);
+function find(id) {
+  return 'a[href="#' + id + '"]';
+}
+$(document).ready(function () {
+  $('main').pushpin({
+    top: $('main').offset().top,
+  });
+  $('.scrollspy').scrollSpy({
+    scrollOfffset: 0,
+  });
+});
+
+// * discover
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.tap-target');
+  var instances = M.TapTarget.init(elems, options);
+});
