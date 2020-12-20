05e9a985c3e986e00012196d93f70325ff95eda6
moulahi-mo <moulahi.mo@gmail.com>
2020-12-20 23:22:19 +0100

    adding services.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 M.AutoInit();
 
+document.querySelectorAll('.row').forEach((item) => {
+  item.classList.add(
+    'animate__animated',
+    'animate__fadeIn',
+    'animate__slower',
+    'wow'
+  );
+});
 // * sidenav
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
@@ -12,26 +20,60 @@ document.addEventListener('DOMContentLoaded', function () {
   var instances = M.Modal.init(elems, options);
 });
 //* scrollspy
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.scrollspy');
-  var instances = M.ScrollSpy.init(elems, options);
-});
-var instance = M.ScrollSpy.getInstance(elem);
-function find(id) {
-  return 'a[href="#' + id + '"]';
-}
-$(document).ready(function () {
-  $('main').pushpin({
-    top: $('main').offset().top,
-  });
-  $('.scrollspy').scrollSpy({
-    scrollOfffset: 0,
-  });
-});
 
 // * discover
 
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.tap-target');
   var instances = M.TapTarget.init(elems, options);
+});
+
+// Scrollfire
+
+// const options = [
+//   {
+//     selector: '.row1',
+//     offset: 50,
+//     callback: function (el) {
+//       Materialize.fadeInImage($(el));
+//     },
+//   },
+//   {
+//     selector: '.row2',
+//     offset: 300,
+//     callback: function (el) {
+//       Materialize.fadeInImage($(el));
+//     },
+//   },
+//   {
+//     selector: '.row3',
+//     offset: 400,
+//     callback: function (el) {
+//       Materialize.fadeInImage($(el));
+//     },
+//   },
+// ];
+
+// Materialize.scrollFire(options);
+
+//* carousel
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+});
+var instance = M.Carousel.init({
+  fullWidth: true,
+  indicators: true,
+});
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true,
+});
+
+//* collapsible
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, options);
 });
